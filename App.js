ae46baf6b280af18e3273e25c34570c12140b4ac
Nizam <ramazanov570633@gmail.com>
2023-12-22 21:41:28 +0400

    Svg path

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Home from './src/Home';
 
 export default function App() {
   return (
@@ -12,8 +13,5 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
--- src/Home.js
+++ src/Home.js
@@ -1,21 +1,30 @@
-import { View, Text, StyleSheet } from 'react-native'
-import React from 'react'
+import React from 'react';
+import { View, StyleSheet, Dimensions,Text } from 'react-native';
+import Svg, { Path } from 'react-native-svg';
+
+const { width, height } = Dimensions.get('window');
 
 const Home = () => {
-  return (
-    <View>
-      <Text>Home</Text>
-    </View>
-  )
-}
+    return (
+        <View style={styles.container}>
+            <Svg width={width} height={height*1.5} viewBox={`0 0 ${width} ${height * 1}`} style={{ position: 'absolute' , marginTop:'-75%'}} >
+                <Path
+                    d={`M${width*0.001} ${height * 0.58} 
+                        C ${width * 0.45} ${height * 0.30}, ${width * 0.5} ${height * 0.8}, ${width} ${height * 0.45}
+                        L ${width} ${height * 0.7}
+                        L 0 ${height * 100} `}
+                    fill="#A8F5C4"
+                />
+            </Svg>
+        </View>
+    );
+};
 
 const styles = StyleSheet.create({
     container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
+        // flex: 1,
+        backgroundColor: '#FFFFFF',
     },
-  });
+});
 
-export default Home
+export default Home;
